bf89a508e83766ec2c8e459bd647df67f95f7e21
Jonathan Hernandez <johnizr1@gmail.com>
2016-06-21 16:29:25 -0700

    Assignment 19 completed

---
+++ controllers/menu_controller.rb
@@ -0,0 +1,64 @@
+require_relative '../models/address_book'
+
+class MenuController
+  attr_reader :address_book
+
+  def initialize
+    @address_book = AddressBook.new
+  end
+
+  def main_menu
+    # #2
+    puts "Main Menu - #{address_book.entries.count} entries"
+    puts "1 - View all entries"
+    puts "2 - Create an entry"
+    puts "3 - Search for an entry"
+    puts "4 - Import entries from a CSV"
+    puts "5 - Exit"
+    print "Enter your selection: "
+
+    # #3
+    selection = gets.to_i
+    # #7
+case selection
+  when 1
+    system "clear"
+    view_all_entries
+    main_menu
+  when 2
+    system "clear"
+    create_entry
+    main_menu
+  when 3
+    system "clear"
+    search_entries
+    main_menu
+  when 4
+    system "clear"
+    read_csv
+    main_menu
+  when 5
+    puts "Good-bye!"
+    # #8
+    exit(0)
+  # #9
+  else
+    system "clear"
+    puts "Sorry, that is not a valid input"
+    main_menu
+end
+end
+
+# #10
+def view_all_entries
+end
+
+def create_entry
+end
+
+def search_entries
+end
+
+def read_csv
+  end
+end
--- models/address_book.rb
+++ models/address_book.rb
@@ -7,17 +7,25 @@ class AddressBook
     @entries = []
   end
 
-    def add_entry(name, phone_number, email)
-      # #9
-      index = 0
-      entries.each do |entry|
-      # #10
-        if name < entry.name
-          break
-        end
-        index += 1
+  def add_entry(name, phone_number, email)
+    # #9
+    index = 0
+    entries.each do |entry|
+    # #10
+      if name < entry.nam
+        break
       end
-      # #11
-      entries.insert(index, Entry.new(name, phone_number, email))
+      index += 1
+    end
+    # #11
+    entries.insert(index, Entry.new(name, phone_number, email))
+  end
+
+  def remove_entry(name, phone_number, email)
+    entries.each do |entry|
+      if entry.name == name || entry.phone_number == phone_number || entry.email == email
+        entries.delete(entry)
+      end
     end
   end
+end
